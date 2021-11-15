ba990c7d2c597433f3b043d5a87e7bfb2f18377a
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-11-15 14:54:27 -0500

    set up route to apply page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { db } from "./firebase/firebase.config";
-import { collection, getDocs } from "firebase/firestore";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import theme from "./theme";
 import { makeStyles } from '@mui/styles';
 import ApplyPage from "./pages/ApplyPage";
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,34 +15,17 @@ function App() {
 
   const classes = useStyles();
 
-  const [applicants, setApplicants] = useState([]);
-  const applicantsCollectionRef = collection(db, "applicants");
-
-  useEffect(() => {
-    const getApplicants = async () => {
-      const data = await getDocs(applicantsCollectionRef);
-      setApplicants(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    }
-
-    getApplicants();
-    console.log('applicants', applicants);
-  }, []);
-
 
   return (
     <ThemeProvider theme={theme}>
       <div className={classes.root}>
-        <ApplyPage />
-        {/* <Router>
+        <Router>
           <Routes>
-            <Route path="/">
-              <div></div>
+            <Route path="/admin" element={<div>Admin page</div>}>
             </Route>
-            <Route exact={true} path="/apply" element={<ApplyPage />}>
-              <ApplyPage />
-            </Route>
+            <Route exact={true} path="/apply" element={<ApplyPage />} />
           </Routes>
-        </Router> */}
+        </Router>
       </div>
     </ThemeProvider>
   );
